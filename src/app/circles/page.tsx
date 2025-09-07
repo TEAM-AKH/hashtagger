6dcf6f8df0417f012090c7b6b7b77facd345fe72
Chi Or <ochi55067@gmail.com>
2025-09-07 19:08:11 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/circles/page.tsx
+++ src/app/circles/page.tsx
@@ -13,6 +13,7 @@ import {
   DialogDescription,
   DialogFooter,
   DialogClose,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
